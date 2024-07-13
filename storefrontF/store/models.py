588cff9f8e3ae029f5f8c16ca17dd02a92d18452
amirb <amirbasuti@gmail.com>
2024-07-13 23:12:56 +0330

    fix bug

--- storefrontF/store/models.py
+++ storefrontF/store/models.py
@@ -9,7 +9,7 @@ class Collection(models.Model):
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    price = models.DecimalField(max_length=6, decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collections = models.ForeignKey(Collection, on_delete=models.CASCADE)
@@ -23,11 +23,12 @@ class Customer(models.Model):
     MEMBERSHIP_BRONZE = 'B'
     MEMBERSHIP_SILVER = 'S'
     MEMBERSHIP_GOLD = 'G'
-    MEMBERSHIP_CHOICES = models.Choices([
-        (MEMBERSHIP_BRONZE, 'Bronze'),
+    MEMBERSHIP_CHOICES =[
+        (MEMBERSHIP_BRONZE, "Bronze"),
         (MEMBERSHIP_SILVER, "Silver"),
         (MEMBERSHIP_GOLD, "Gold"),
-    ])
+    ]
+
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
@@ -36,24 +37,23 @@ class Customer(models.Model):
     birth_date = models.DateField(null=True)
 
 
-class Order(models.Model):
-    PEYMENT_STATUS_PENDING = 'P'
-    PEYMENT_STATUS_COMPLETE = 'C'
-    PEYMENT_STATUS_FAILD = 'F'
-    PEYMENT_STATUS = models.Choices([
-        (PEYMENT_STATUS_PENDING, "Pending"),
-        (PEYMENT_STATUS_COMPLETE, "Complete"),
-        (PEYMENT_STATUS_FAILD, "Failed")
-    ])
-    placed_at = models.DateField(auto_now_add=True)
-    peyment_status = models.CharField(max_length=1, choices=PEYMENT_STATUS, default=PEYMENT_STATUS_PENDING)
-    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
-
-
 class Adress(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+class Order(models.Model):
+    PEYMENT_STATUS_PENDING = 'P'
+    PEYMENT_STATUS_COMPLETE = 'C'
+    PEYMENT_STATUS_FAILD = 'F'
+    PEYMENT_STATUS = [
+        (PEYMENT_STATUS_PENDING, "Pending"),
+        (PEYMENT_STATUS_COMPLETE, "Complete"),
+        (PEYMENT_STATUS_FAILD, "Failed")
+    ]
+    placed_at = models.DateField(auto_now_add=True)
+    peyment_status = models.CharField(max_length=1, choices=PEYMENT_STATUS, default=PEYMENT_STATUS_PENDING)
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
 
 
 class Item(models.Model):
@@ -65,7 +65,7 @@ class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.PROTECT)
     product = models.ForeignKey(Product, on_delete=models.PROTECT)
     quantity = models.PositiveSmallIntegerField()
-    unit_price = models.DecimalField(max_length=6, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=5, decimal_places=2)
 
 
 class CartItem(models.Model):
