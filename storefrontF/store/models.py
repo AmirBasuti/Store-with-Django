fb27ebd451da139b457b19d364755bd9a0dd03b9
amirb <amirbasuti@gmail.com>
2024-07-14 03:27:51 +0330

    add zip to addres

--- storefrontF/store/models.py
+++ storefrontF/store/models.py
@@ -43,6 +43,7 @@ class Customer(models.Model):
 
 
 class Adress(models.Model):
+    zip = models.CharField(max_length=255, null=True)
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
