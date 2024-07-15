2cd80a86783686334559fcf3b22eda29cdbcad60
amirb <amirbasuti@gmail.com>
2024-07-15 22:38:25 +0330

    select related object

--- storefrontF/playground/views.py
+++ storefrontF/playground/views.py
@@ -2,20 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from store.models import Customer, Product, Collection, Promotion, Cart, Order, Adress, OrderItem
-from django.db.models import Q, F
+from django.db.models import Q, F, aggregates
 
 
 
 
 def say_hello(request):
-    # object = Product.objects.filter(unit_price__gt=20.0).order_by('title')
-    # object = Customer.objects.filter(email__icontains='.com')
-    # object = Collection.objects.filter(featured_product__isnull=True)
-    # object = Product.objects.filter(inventory__lt=10)
-    # object = Order.objects.filter(customer_id=1)
+    object = Order.objects.select_related("customer").prefetch_related('orderitem_set__product').order_by('placed_at')[:5]
     # print(object)
-    # object = Product.objects.filter(Q(inventory__lt=10) | Q(unit_price__lt=20.0))
-    # object = Product.objects.filter( Q(inventory = F('unit_price')))
-    object = Product.objects.filter(id__in=OrderItem.objects.values('product_id').distinct())
     template = loader.get_template('hello.html')
     return HttpResponse(template.render({'object': object}, request))
