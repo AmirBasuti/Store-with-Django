6d8c494b54a0a385ea7a7642bdc0d75cfd927fe9
amirb <amirbasuti@gmail.com>
2024-07-15 23:23:36 +0330

    Aggregating Exercises

--- storefrontF/playground/views.py
+++ storefrontF/playground/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.db.models import Q, F
+from django.db.models.aggregates import Count, Sum, Avg, Max, Min
 from store.models import Customer, Product, Collection, Promotion, Cart, Order, Adress, OrderItem
-from django.db.models import Q, F, aggregates
+
 
 
 
 
 def say_hello(request):
-    object = Order.objects.select_related("customer").prefetch_related('orderitem_set__product').order_by('placed_at')[:5]
-    # print(object)
+
+    # resualt = Order.objects.aggregate(count = Count(id))
+    # resualt = OrderItem.objects.filter(product_id= 1).aggregate(count = Sum('quantity'))
+    # resualt = Order.objects.filter(customer_id= 1).aggregate(count = Count('id'))
+    resualt = Product.objects.filter(collection__id= 3).aggregate(Min = Min('unit_price'), Max = Max('unit_price'), Avg = Avg('unit_price'))
+    print(resualt)
     template = loader.get_template('hello.html')
-    return HttpResponse(template.render({'object': object}, request))
+    return HttpResponse(template.render({'resualt': resualt}, request))
