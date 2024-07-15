90fa6edc9b37355afd4e682246ab39b4c81b41bb
amirb <amirbasuti@gmail.com>
2024-07-15 19:13:51 +0330

    OR IN QUERRY

--- storefrontF/playground/views.py
+++ storefrontF/playground/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from store.models import Customer, Product, Collection, Promotion, Cart, Order, Adress
+from django.db.models import Q
 
 
 
@@ -12,7 +13,7 @@ def say_hello(request):
     # object = Collection.objects.filter(featured_product__isnull=True)
     # object = Product.objects.filter(inventory__lt=10)
     # object = Order.objects.filter(customer_id=1)
-    print(object)
-
+    # print(object)
+    object = Product.objects.filter(Q(inventory__lt=10) | Q(unit_price__lt=20.0))
     template = loader.get_template('hello.html')
     return HttpResponse(template.render({'object': object}, request))
