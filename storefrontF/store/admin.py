879d96fceb7daba4be14638df7642e570c786336
amirb <amirbasuti@gmail.com>
2024-07-16 20:46:16 +0330

    fuc hard modify creat class in admin.py app and setup decorative and modify admin interface so easy

--- storefrontF/store/admin.py
+++ storefrontF/store/admin.py
@@ -1,7 +1,34 @@
 from django.contrib import admin
 
-from store.models import Product, Collection
+from store.models import Product, Collection, Customer
 
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description', 'unit_price']
+    list_editable = ['unit_price']
+    list_per_page = 10
+    search_fields = ['title', 'description']
+    list_filter = ['last_update', 'collection']
+    date_hierarchy = 'last_update'
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'membership']
+    list_editable = ['membership']
+    list_per_page = 10
+    search_fields = ['first_name', 'last_name']
+    list_filter = ['membership']
+    ordering = ['first_name', 'last_name']
 # Register your models here.
-admin.site.register(Collection)
-admin.site.register(Product)
+
+@admin.register(Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ['title', 'featured_product']
+    list_editable = ['featured_product']
+    list_per_page = 10
+    search_fields = ['title']
+    ordering = ['title']
+# admin.site.register(Collection)
+# /admin.site.register(Product)
