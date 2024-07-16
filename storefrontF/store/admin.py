ccead834535a0f23da4373e479bb6954a82448a3
amirb <amirbasuti@gmail.com>
2024-07-16 22:12:59 +0330

    select realated object

--- storefrontF/store/admin.py
+++ storefrontF/store/admin.py
@@ -1,17 +1,22 @@
 from django.contrib import admin
 
-from store.models import Product, Collection, Customer
+from store.models import Product, Collection, Customer, Order
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price', 'inventory_status']
+    list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
     list_editable = ['unit_price']
     list_per_page = 10
     search_fields = ['title', 'description']
     list_filter = ['last_update', 'collection']
     date_hierarchy = 'last_update'
     # ordering = ['inventory']
+    list_select_related = ['collection']
+
+    def collection_title(self, product):
+        return product.collection.title
+
     @admin.display(ordering='inventory')
     def inventory_status(self, product):
         if product.inventory < 10:
@@ -38,5 +43,19 @@ class CollectionAdmin(admin.ModelAdmin):
     list_per_page = 10
     search_fields = ['title']
     ordering = ['title']
+
+
 # admin.site.register(Collection)
 # /admin.site.register(Product)
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'placed_at', 'customer']
+    list_select_related = ['customer']
+    list_per_page = 10
+    search_fields = ['customer__first_name', 'customer__last_name']
+    # ordering = ['placed_at']
+    ordering = ['customer']
+    # @admin.display(ordering='placed_at')
+    # def customer_name(self, order):
+    #     return f'{order.customer.first_name} {order.customer.last_name}'
--- storefrontF/store/models.py
+++ storefrontF/store/models.py
@@ -57,6 +57,12 @@ class Customer(models.Model):
     phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True)
 
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+    class Meta:
+        ordering = ['first_name', 'last_name']
+
 
 class Adress(models.Model):
     # zip = models.CharField(max_length=255, null=True)
