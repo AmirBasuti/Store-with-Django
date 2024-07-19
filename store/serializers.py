c6e8ef7aee15bc40ef9c3cc85001f2b388b15bb7
amirb <amirbasuti@gmail.com>
2024-07-20 00:49:22 +0330

    serializing relationship first we use primarykeyrelated and get id second stringrealted name using --str-- func to get name of related third we define collection serializer and pass at the field forth and challenger is put url text in it found out we can use name for the url and call it when ever we want NOTE THAT FOR SOME FIELD WE SHOULD DEFINE QUERYSET

--- store/serializers.py
+++ store/serializers.py
@@ -2,12 +2,22 @@ from decimal import Decimal
 
 from rest_framework import serializers
 
+from store.models import Collection
+
+class CollectionSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField(max_length=255)
+
 
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
-    title = serializers.CharField(max_length=200)
+    title = serializers.CharField(max_length=255)
     price = serializers.DecimalField(max_digits=10, decimal_places=2, source='unit_price')
     price_with_tax = serializers.SerializerMethodField(method_name='cal_price_with_tax')
-
+    # collection = serializers.PrimaryKeyRelatedField(queryset=Collection.objects.all())
+    # collection = serializers.StringRelatedField()
+    # collection = CollectionSerializer()
+    collection = serializers.HyperlinkedRelatedField(
+        view_name='collection-detail', read_only=True)
     def cal_price_with_tax(self, product):
         return round(product.unit_price * Decimal(1.1), 2)
--- store/views.py
+++ store/views.py
@@ -10,12 +10,18 @@ from store.serializers import ProductSerializer
 # Create your views here.
 @api_view()
 def product_list(request):
-    serializer = ProductSerializer(Product.objects.all(), many=True)
+    queryset = Product.objects.select_related('collection').all()
+    serializer = ProductSerializer(queryset, many=True, context={'request': request})
     return Response(serializer.data)
 
 
 @api_view()
 def product_detail(request, id):
     product = get_object_or_404(Product, pk=id)
-    serializer = ProductSerializer(product)
+    serializer = ProductSerializer(product, context={'request': request})
     return Response(serializer.data)
+
+
+@api_view()
+def collection_detail(request, pk):
+    return Response('ok')
