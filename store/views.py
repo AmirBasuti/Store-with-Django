927235a30f3d4e56152ca02cf2fe1cbc45274c4e
amirb <amirbasuti@gmail.com>
2024-07-19 21:24:51 +0330

    serializing object serializer get object or query set use shortcut to get object or 404 error change settings.py to don't convert decimal to string

--- store/views.py
+++ store/views.py
@@ -1,15 +1,21 @@
 # from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
+from store.models import Product
+from store.serializers import ProductSerializer
 
 
 # Create your views here.
 @api_view()
 def product_list(request):
-    # return HttpResponse("Hello, World!")
-    return Response("Hello, World!")
+    serializer = ProductSerializer(Product.objects.all(), many=True)
+    return Response(serializer.data)
 
 
 @api_view()
 def product_detail(request, id):
-    return Response(id)
+    product = get_object_or_404(Product, pk=id)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
