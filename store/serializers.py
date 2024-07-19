06a20fb4d457fc7a38b30e563f75a49ffc6b43b0
amirb <amirbasuti@gmail.com>
2024-07-20 01:49:28 +0330

    Model serializers

--- store/serializers.py
+++ store/serializers.py
@@ -2,22 +2,22 @@ from decimal import Decimal
 
 from rest_framework import serializers
 
-from store.models import Collection
-
-class CollectionSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
+from store.models import Collection, Product
 
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    price = serializers.DecimalField(max_digits=10, decimal_places=2, source='unit_price')
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ['id', 'title', 'featured_product']
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'unit_price', 'price_with_tax', 'collection']
+
     price_with_tax = serializers.SerializerMethodField(method_name='cal_price_with_tax')
-    # collection = serializers.PrimaryKeyRelatedField(queryset=Collection.objects.all())
-    # collection = serializers.StringRelatedField()
-    # collection = CollectionSerializer()
-    collection = serializers.HyperlinkedRelatedField(
-        view_name='collection-detail', read_only=True)
-    def cal_price_with_tax(self, product):
+
+    @staticmethod
+    def cal_price_with_tax(product):
         return round(product.unit_price * Decimal(1.1), 2)
