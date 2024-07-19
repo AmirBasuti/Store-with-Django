423510ad9f72d657f12f58dc89b4b0df550955dd
amirb <amirbasuti@gmail.com>
2024-07-19 21:42:08 +0330

    custom serializers.py

--- store/serializers.py
+++ store/serializers.py
@@ -1,7 +1,13 @@
+from decimal import Decimal
+
 from rest_framework import serializers
 
 
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=200)
-    unit_price = serializers.DecimalField(max_digits=10, decimal_places=2)
+    price = serializers.DecimalField(max_digits=10, decimal_places=2, source='unit_price')
+    price_with_tax = serializers.SerializerMethodField(method_name='cal_price_with_tax')
+
+    def cal_price_with_tax(self, product):
+        return round(product.unit_price * Decimal(1.1), 2)
