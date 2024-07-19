4008e6288f6dd94878cbd81cf01d3545f3adcca5
amirb <amirbasuti@gmail.com>
2024-07-20 02:54:46 +0330

    Saving object overwrite validation(if you should validate some addition validation)
    creat (if you want to add somthing is not in input)
    
    update
    implement put in product
    
    set status and data to return

--- store/serializers.py
+++ store/serializers.py
@@ -14,10 +14,28 @@ class CollectionSerializer(serializers.ModelSerializer):
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['id', 'title', 'unit_price', 'price_with_tax', 'collection']
+        fields = ['id', 'title', 'description', 'slug', 'unit_price', 'inventory', 'price_with_tax', 'collection']
 
     price_with_tax = serializers.SerializerMethodField(method_name='cal_price_with_tax')
 
     @staticmethod
     def cal_price_with_tax(product):
         return round(product.unit_price * Decimal(1.1), 2)
+
+    #override the validate method
+    # def validat(self, data):
+    #     if data['password'] != data['password_confirm']:
+    #         raise serializers.ValidationError('Passwords do not match')
+    #
+
+    # def create(self, validated_data):
+    #     product = Product(**validated_data)
+    #     product.other_field = 'other value'
+    #     product.save()
+    #     return product
+
+    # def update(self, instance, validated_data):
+    #     instance.title = validated_data.get('title', instance.title)
+    #     instance.description = validated_data.get('description', instance.description)
+    #     instance.save()
+    #     return instance
