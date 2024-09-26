d6323c353a30f8badcd98382b1fe900fda483dbb
amirb <amirbasuti@gmail.com>
2024-09-26 22:19:45 +0330

    using view  set for combine 2 view in the same class to reduce duplication in the class its like generic view set. we should behave like that

--- store/views.py
+++ store/views.py
@@ -4,6 +4,7 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin
+from rest_framework.viewsets import ModelViewSet
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -13,21 +14,13 @@ from store.serializers import ProductSerializer, CollectionSerializer
 
 
 # Create your views here.
-class ProductList(ListCreateAPIView):
-    queryset = Product.objects.all()
+class ProductViewSet(ModelViewSet):
 
+    queryset = Product.objects.select_related.all()
     serializer_class = ProductSerializer
 
     def get_serializer_context(self):
-        return {'request':self.request}
-
-
-class ProductDetail(RetrieveUpdateDestroyAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-
-    def get_serializer_context(self):
-        return {'request':self.request}
+        return {'request': self.request}
 
     def delete(self, request: Request, pk):
         product = get_object_or_404(Product, pk=pk)
@@ -35,8 +28,32 @@ class ProductDetail(RetrieveUpdateDestroyAPIView):
             return Response({'error': 'Product cannot be deleted because it is associated with an order item'},
                             status=status.HTTP_405_METHOD_NOT_ALLOWED)
         product.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
 
+#
+# class ProductList(ListCreateAPIView):
+#     queryset = Product.objects.select_related.all()
+#
+#     serializer_class = ProductSerializer
+#
+#     def get_serializer_context(self):
+#         return {'request':self.request}
+#
+#
+# class ProductDetail(RetrieveUpdateDestroyAPIView):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
+#
+#     def get_serializer_context(self):
+#         return {'request':self.request}
+#
+#     def delete(self, request: Request, pk):
+#         product = get_object_or_404(Product, pk=pk)
+#         if product.orderitems.exists():
+#             return Response({'error': 'Product cannot be deleted because it is associated with an order item'},
+#                             status=status.HTTP_405_METHOD_NOT_ALLOWED)
+#         product.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+#
 
 class CollectionList(ListCreateAPIView):
     queryset = Collection.objects.prefetch_related('products').all()
