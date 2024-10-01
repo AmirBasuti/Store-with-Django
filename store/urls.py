8515748e90e884a45fbcc13fd4343be0d1a367b0
amirb <amirbasuti@gmail.com>
2024-10-02 00:54:55 +0330

    creat neste router for the product viewset https://github.com/alanjds/drf-nested-routers

--- store/urls.py
+++ store/urls.py
@@ -1,15 +1,19 @@
+from itertools import product
+
 from django.urls import path, include
 
 from store.views import *
-from rest_framework import routers
-
+# from rest_framework import routers
+from rest_framework_nested import routers
 router = routers.DefaultRouter() # Simplerouter
 router.register('products', ProductViewSet)
 router.register('collection', CollectionViewSet)
+product_router = routers.NestedDefaultRouter(router, 'products', lookup='product')
+product_router.register('reviews', viewset= ReviewViewSet, basename='product-reviews')
 # urlpatterns = [
 #     path('', include(router.urls))
 # ]
-urlpatterns = router.urls
+urlpatterns = router.urls + product_router.urls
 # urlpatterns = [
 #     path("products/", ProductList.as_view(), name='product-list'),
 #     path("products/<int:pk>/", ProductDetail.as_view(), name='product-detail'),
